67ab9ab6adc1084c25a7622bc73a82385c01e1a8
Haley Myers <haleynmyers@gmail.com>
2020-10-13 15:44:27 -0500

    Creared db.js

---
+++ public/db.js
@@ -0,0 +1,58 @@
+let db;
+const request = window.indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  db = event.target.result;
+  const pending = db.createObjectStore("pending", { autoIncrement: true });
+  pending.createIndex("transactionName", "name");
+  pending.createIndex("transactionValue", "value");
+  pending.createIndex("transactionDate", "date");
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.error);
+};
+
+function saveRecord(record) {
+  const db = request.result;
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+  pendingStore.add(record);
+}
+
+function checkDatabase() {
+  const db = request.result;
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+  const getAll = pendingStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const db = request.result;
+          const transaction = db.transaction(["budget"], "readwrite");
+          const pendingStore = transaction.objectStore("pending");
+          pendingStore.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
